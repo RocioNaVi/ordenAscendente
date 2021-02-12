7f166892ad3d0716026b73b4be8aa0dd75347f45
RocioNaVi <r.navarro.2016@alumnos.urjc.es>
2021-02-12 17:00:09 +0100

    Primer metodo ordenado

---
+++ ordenAscendente.java
@@ -0,0 +1,19 @@
+package Examen2021;
+
+public class ordenAscendente {
+
+	public static Boolean ordenado (int[] x)
+	{
+		int n = 0;
+		for (int i=0; i < x.length; i++)
+		{
+			if (x[i] >= n)
+			{
+				n = x[i];
+			}else {
+				return false;
+			}
+		}
+		return true;
+	}
+}
