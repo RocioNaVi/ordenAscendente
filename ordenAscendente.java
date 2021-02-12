29e4b4a83858c2a313b4657acd628dca7b64d744
RocioNaVi <r.navarro.2016@alumnos.urjc.es>
2021-02-12 17:14:30 +0100

    Corregido Boolean

--- ordenAscendente.java
+++ ordenAscendente.java
@@ -2,7 +2,7 @@ package Examen2021;
 
 public class ordenAscendente {
 
-	public static Boolean ordenado (int[] x)
+	public static boolean ordenado (int[] x)
 	{
 		int n = 0;
 		for (int i=0; i < x.length; i++)
